3708bab7a2e7cca82b5f2def5fc8febb9bc0e5cb
candyolivia <candyoliviamawalim@gmail.com>
2017-03-29 15:36:29 +0700

    similarity finder

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, jsonify
 from chatter_bot import ChatterBot
+from similarity_finder import SimilarityFinder
+import os
 
 app = Flask(__name__)
 cb = ChatterBot()
+sf = SimilarityFinder()
+
+mydir = os.path.dirname(__file__)
+file = os.path.join(mydir, 'movies_list.txt')
 
 @app.route('/get-reply', methods=['POST'])
 def get_reply():
@@ -10,6 +16,15 @@ def get_reply():
 	message = json["message"]
 	reply = cb.get_reply(message)
 	result = {'reply': str(reply)}
+	return jsonify(result)
+
+@app.route('/get-title', methods=['POST'])
+def get_title():
+	json = request.json
+	message = json["message"]
+	titles = sf.getTitleFromTitleList(file)
+	title = sf.findMostSimilarTitle(message, titles)
+	result = {'title': str(title)}
 	return jsonify(result)
 
 if __name__ == '__main__':
---
+++ similarity_finder.py
@@ -0,0 +1,48 @@
+from difflib import SequenceMatcher
+
+class SimilarityFinder:
+	"""docstring for SimilarityFinder"""
+	def similar(self, a,b):
+		return SequenceMatcher(None, a, b).ratio()
+
+	def getTitleFromTitleList(self, filename):
+		file = open(filename,'r')
+		titles = file.readlines()
+		return titles
+
+	def findMostSimilarTitle(self, str, titles):
+		max_idx = 0
+		maximum = 0
+		found = 0
+		res = ""
+		for title in titles:
+			title = title.replace("\n","")
+			if str.find(title) != -1:
+				res = title
+				found = 1
+				break
+			elif str.find(title.split(":")[0]) != -1:
+				res = title
+				found = 1
+				break
+
+		if found == 0:
+			for i, title in enumerate(titles):
+				if maximum < similar(str, title):
+					maximum = similar(str, title)
+					max_idx = i
+
+				if title.find(":") != -1:
+					firstWord = title.split(":")[0]
+					if maximum < similar(str, firstWord):
+						maximum = similar(str, firstWord)
+						max_idx = i
+			if maximum > 0.4:
+				res = titles[max_idx]
+
+		return res
+
+if __name__ == '__main__':
+	sf = SimilarityFinder()
+	titles = sf.getTitleFromTitleList("movies_list.txt")
+	print sf.findMostSimilarTitle("mau nonton Logan", titles)
