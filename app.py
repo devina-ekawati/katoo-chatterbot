56122e765a88e5ab8ef5d1f0ddf4f1ac71b69082
Devina Ekawati <devina.ekawati@gmail.com>
2017-04-02 22:24:46 +0700

    Add restaurant name and movie name finder, fix routing

--- app.py
+++ app.py
@@ -1,31 +1,62 @@
 from flask import Flask, request, jsonify
 from chatter_bot import ChatterBot
-from similarity_finder import SimilarityFinder
-import os
+from movie_name_finder import MovieNameFinder
+from restaurant_name_finder import RestaurantNameFinder
+import os, requests
 
 app = Flask(__name__)
-mydir = os.path.dirname(__file__)
+my_dir = os.path.dirname(__file__)
 
-cb = ChatterBot()
-sf = SimilarityFinder()
+# cb = ChatterBot()
+mf = MovieNameFinder()
+rf = RestaurantNameFinder()
+
+env = {}
+file = os.path.join(my_dir, '.env')
+with open(file, "r") as f:
+	for line in f:
+		line = line.rstrip()
+		tokens = line.split("=")
+		env[tokens[0]] = tokens[1]
 
 @app.route('/get-reply', methods=['POST'])
 def get_reply():
 	json = request.json
 	message = json["message"]
-	reply = cb.get_reply(message)
-	result = {'reply': str(reply)}
+	reply = {}
+	movie_name = mf.get_movie_name(message)
+	if not movie_name:
+		location = get_location(message)
+		restaurant_name = rf.get_restaurant_name(message, location)
+
+		if not restaurant_name:
+			# reply = {"code": 3, "name": cb.get_reply(message)}
+			reply = {"code": 3, "name": "chatterbot"}
+		else:
+			reply = {"code": 1, "name": restaurant_name, "location": location}	
+	else:
+		reply = {"code": 2, "name": movie_name}
+
+	result = {'reply': reply}
 	return jsonify(result)
 
-@app.route('/get-title', methods=['POST'])
-def get_title():
-	json = request.json
-	message = json["message"]
-	file = os.path.join(mydir, 'movies_list.txt')
-	titles = sf.getTitleFromTitleList(file)
-	title = sf.findMostSimilarTitle(message, titles)
-	result = {'title': str(title)}
-	return jsonify(result)
+def connect_to_kata_ai(text):
+	text = "+".join(text.split())
+	url = "https://api.kata.ai/v1/insights"
+	headers = {"Authorization": env["API_KATA_AI"]}
+	param = {'m': text}
+	r = requests.get(url, params=param, headers=headers)
+	return r.json()
+
+def get_location(text):
+	result = connect_to_kata_ai(text)
+	location = ""
+	if (result["code"] == 200):
+		entities = result["entities"]
+		for entity in entities:
+			if entity["entity"] == "LOCATION":
+				location = entity["fragment"]
+	return location
 
 if __name__ == '__main__':
     app.run()
--- similarity_finder.py
+++ similarity_finder.py
@@ -2,47 +2,48 @@ from difflib import SequenceMatcher
 
 class SimilarityFinder:
 	"""docstring for SimilarityFinder"""
+	def __init__(self, items):
+		self.items = items
+
 	def similar(self, a,b):
 		return SequenceMatcher(None, a, b).ratio()
 
-	def getTitleFromTitleList(self, filename):
-		file = open(filename,'r')
-		titles = file.readlines()
-		return titles
-
-	def findMostSimilarTitle(self, str, titles):
+	def findMostSimilarItem(self, str):
 		max_idx = 0
 		maximum = 0
 		found = 0
 		res = ""
-		for title in titles:
-			title = title.replace("\n","")
-			if str.find(title) != -1:
-				res = title
+		for item in self.items:
+			item = item.replace("\n","")
+			if str.find(item) != -1:
+				res = item
 				found = 1
 				break
-			elif str.find(title.split(":")[0]) != -1:
-				res = title
+			elif str.find(item.split(":")[0]) != -1:
+				res = item
 				found = 1
 				break
 
 		if found == 0:
-			for i, title in enumerate(titles):
-				if maximum < self.similar(str, title):
-					maximum = self.similar(str, title)
+			for i, item in enumerate(self.items):
+				if maximum < self.similar(str, item):
+					maximum = self.similar(str, item)
 					max_idx = i
 
-				if title.find(":") != -1:
-					firstWord = title.split(":")[0]
+				if item.find(":") != -1:
+					firstWord = item.split(":")[0]
 					if maximum < self.similar(str, firstWord):
 						maximum = self.similar(str, firstWord)
 						max_idx = i
 			if maximum > 0.4:
-				res = titles[max_idx]
+				res = self.items[max_idx]
 
 		return res
 
 if __name__ == '__main__':
-	sf = SimilarityFinder()
-	titles = sf.getTitleFromTitleList("movies_list.txt")
-	print sf.findMostSimilarTitle("mau nonton Logan", titles)
+	file = open("movies_list.txt",'r')
+	titles = file.readlines()
+
+	sf = SimilarityFinder(titles)
+
+	print sf.findMostSimilarItem("mau nonton Beauty and the beast")
